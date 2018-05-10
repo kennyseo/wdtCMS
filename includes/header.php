b4ba886b3167e1f11085997db80aff881e77f7e8
kennykseo <kenny.seo@websitedevteam.com>
2018-05-10 14:04:08 -0500

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- <meta echo > -->
+<title>Textbooks - U of A Bookstore</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="page" content="textbook">
+
+<style>
+@import 'https://fonts.googleapis.com/css?family=Open+Sans';
+@import 'https://fonts.googleapis.com/css?family=Tangerine';
+</style>
+<link href='/update/include/style2.css' rel='stylesheet' type='text/css'>
+<link href='/update/include/style.css' rel='stylesheet' type='text/css'>
+<link href='/update/include/style3.css' rel='stylesheet' type='text/css'>
+<link rel="shortcut icon" href="https://uofastore.com/images/uofabookstore/favicon2.ico" type="https://uofastore.com/images/uofabookstore/favicons.ico"/>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+<script src="/update/include/jquery.min.js"></script>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,100,700" />
+
+
+<!-- FAQ  -->
+<link rel='stylesheet' href='/update/include/faq.css'>
+<script src="/update/include/modernizr.js"></script>
+<script src="/update/include/jquery-2.1.1.js"></script>
+<!-- <script src="/update/includes/jquery.mobile.custom.min.js"></script> -->
+<!-- <script src="/update/include/faq-main.js"></script> -->
+
+<!-- Table  -->
+<link rel='stylesheet' href='/update/include/table/responsive-tables.css'>
+<script src="/update/include/table/responsive-tables.js"></script>
+<script src="https://uofastore.com/update/include/faq-main.js"></script>
+
+</head>
+<body id="totop">
