ce2d5896db96849ddf0833d5f1f464bb1bf9a771
kennykseo <kenny.seo@websitedevteam.com>
2018-05-10 14:13:58 -0500

    ignore

--- includes/header.php
+++ includes/header.php
@@ -14,30 +14,30 @@
 @import 'https://fonts.googleapis.com/css?family=Open+Sans';
 @import 'https://fonts.googleapis.com/css?family=Tangerine';
 </style>
-<link href='/update/include/style2.css' rel='stylesheet' type='text/css'>
-<link href='/update/include/style.css' rel='stylesheet' type='text/css'>
-<link href='/update/include/style3.css' rel='stylesheet' type='text/css'>
+<link href='https://uofastore.com/update/include/style2.css' rel='stylesheet' type='text/css'>
+<link href='https://uofastore.com/update/include/style.css' rel='stylesheet' type='text/css'>
+<link href='https://uofastore.com/update/include/style3.css' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="https://uofastore.com/images/uofabookstore/favicon2.ico" type="https://uofastore.com/images/uofabookstore/favicons.ico"/>
 
 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
-<script src="/update/include/jquery.min.js"></script>
+<script src="https://uofastore.com/update/include/jquery.min.js"></script>
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,100,700" />
 
 
 <!-- FAQ  -->
-<link rel='stylesheet' href='/update/include/faq.css'>
-<script src="/update/include/modernizr.js"></script>
-<script src="/update/include/jquery-2.1.1.js"></script>
-<!-- <script src="/update/includes/jquery.mobile.custom.min.js"></script> -->
-<!-- <script src="/update/include/faq-main.js"></script> -->
+<link rel='stylesheet' href='https://uofastore.com/update/include/faq.css'>
+<script src="https://uofastore.com/update/include/modernizr.js"></script>
+<script src="https://uofastore.com/update/include/jquery-2.1.1.js"></script>
+<!-- <script src="https://uofastore.com/update/includes/jquery.mobile.custom.min.js"></script> -->
+<!-- <script src="https://uofastore.com/update/include/faq-main.js"></script> -->
 
 <!-- Table  -->
-<link rel='stylesheet' href='/update/include/table/responsive-tables.css'>
-<script src="/update/include/table/responsive-tables.js"></script>
+<link rel='stylesheet' href='https://uofastore.com/update/include/table/responsive-tables.css'>
+<script src="https://uofastore.com/update/include/table/responsive-tables.js"></script>
 <script src="https://uofastore.com/update/include/faq-main.js"></script>
 
 </head>
